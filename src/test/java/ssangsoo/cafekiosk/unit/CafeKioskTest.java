5be621d03e19fd47f2dd76f9552c2cfe8ac614c5
SsangSoo <kss10080@naver.com>
2025-04-03 09:06:35 +0900

    section 05 end

--- src/test/java/ssangsoo/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/ssangsoo/cafekiosk/unit/CafeKioskTest.java
@@ -1,6 +1,6 @@
 package ssangsoo.cafekiosk.unit;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ssangsoo.cafekiosk.unit.beverages.Americano;
 import ssangsoo.cafekiosk.unit.beverages.Latte;
@@ -21,6 +21,8 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+//    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -79,8 +81,10 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -88,10 +92,15 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
+
+
 
     @Test
     void createOrder() {
