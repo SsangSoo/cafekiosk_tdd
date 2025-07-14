df4c6d9cc51a22cc4d9ea50adfb5b79bcab2333f
SsangSoo <kss10080@naver.com>
2025-07-15 07:15:26 +0900

    section 07 complete

--- src/test/java/ssangsoo/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/ssangsoo/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,8 +3,7 @@ package ssangsoo.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ssangsoo.cafekiosk.spring.client.mail.MailSendClient;
 import ssangsoo.cafekiosk.spring.domain.history.Mail.MailSendHistory;
@@ -13,6 +12,7 @@ import ssangsoo.cafekiosk.spring.domain.history.Mail.MailSendHistoryRepository;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,10 +31,12 @@ class MailServiceTest {
     @DisplayName("메일 전송 테스트")
     void sendMail() {
         // given
-        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(true);
+//        Mockito.when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+//                .thenReturn(true);
+        given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
 
-//        @Spy 사용시
+          // @Spy 사용시
 //        doReturn(true)
 //                .when(mailSendClient)
 //                .sendEmail(anyString(), anyString(), anyString(), anyString());
@@ -45,6 +47,7 @@ class MailServiceTest {
         // then
         assertThat(result).isTrue();
         verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class)); // veryfy는 메서드가 수행되고 나야 체킹이 되기 때문에, then에 와야 한다.
-
     }
+
+
 }
